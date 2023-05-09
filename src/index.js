4982c5636edefd291db3cbaeaa0d20416c49b1e5
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2023-05-09 12:08:25 -0400

    axios added

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+import debounce from "lodash.debounce";
+
+refs = {
+  inputEl: document.querySelector("#search-form"),
+};
+
+API_KEY = process.env.API_KEY;
+BASE_URL = `https://pixabay.com/api/?key=`;
+let queryParam;
+console.log(queryParam);
+async function getPictures(queryParameters) {
+  console.log("insede api call " + queryParameters);
+
+  console.log(`${BASE_URL}${API_KEY}&q=${queryParameters}`);
+  return await axios.get(`${BASE_URL}${API_KEY}&q=${queryParameters}`);
+}
+const onInput = (e) => {
+  e.preventDefault();
+  const { searchQuery } = e.target.elements;
+  queryParam = searchQuery.value;
+  getPictures(queryParam).then(({data})=>console.log(data));
+};
+
+
+refs.inputEl.addEventListener("submit", onInput);
