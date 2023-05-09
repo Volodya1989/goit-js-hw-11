4bc084aa8a97ba46cff7a917bbd39b0f058f8f64
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2023-05-09 12:29:46 -0400

    picture now is displayed

--- src/index.js
+++ src/index.js
@@ -1,28 +1,57 @@
-// import axios from "axios";
 // import debounce from "lodash.debounce";
 import API from "./apiCalls.js";
 
 refs = {
   inputEl: document.querySelector("#search-form"),
+  gallery: document.querySelector(".gallery"),
+};
+const markupOfPictures = (data) => {
+  const { hits } = data;
+  const markup = hits
+    .map(
+      ({
+        id,
+        comments,
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        downloads,
+      }) => {
+        return `<div data-id=${id} class="photo-card">
+  <img src=${webformatURL} alt="${tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes: ${likes}</b>
+    </p>
+    <p class="info-item">
+      <b>Views: ${views}</b>
+    </p>
+    <p class="info-item">
+      <b>Comments: ${comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads: ${downloads}</b>
+    </p>
+  </div>
+</div>`;
+      }
+    )
+    .join("");
+  refs.gallery.insertAdjacentHTML("afterbegin", markup);
 };
 
-// API_KEY = process.env.API_KEY;
-// BASE_URL = `https://pixabay.com/api/?key=`;
-// let queryParam;
-// console.log(queryParam);
-// async function getPictures(queryParameters) {
-//   console.log("insede api call " + queryParameters);
-
-//   console.log(`${BASE_URL}${API_KEY}&q=${queryParameters}`);
-//   return await axios.get(`${BASE_URL}${API_KEY}&q=${queryParameters}`);
-// }
 const onInput = (e) => {
   e.preventDefault();
 
   const { searchQuery } = e.target.elements;
   const queryParam = searchQuery.value;
-    API.getPictures(queryParam).then(({ data }) => console.log(data));
-    searchQuery.value = "";
+  API.getPictures(queryParam).then(({ data }) => {
+    console.log(data.hits);
+    markupOfPictures(data);
+  });
+  searchQuery.value = "";
 };
 
 refs.inputEl.addEventListener("submit", onInput);
