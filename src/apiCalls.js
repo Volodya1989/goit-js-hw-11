e572ed8286d7d8735616062e1ea78ef17ca8bf7e
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2023-05-10 02:15:17 -0400

    api updated

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-API_KEY = `36217922-a5a1e017917ad4e4c89a8ab46`;
+API_KEY = "36217922-a5a1e017917ad4e4c89a8ab46";
 BASE_URL = `https://pixabay.com/api/?key=`;
 PARAMS = `&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=`;
 async function getPictures(searchKeyWord, pageNumber) {
