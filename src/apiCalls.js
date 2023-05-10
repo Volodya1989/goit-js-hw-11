41f72683c0746ee08fff820560e05b7102280d99
Volodymyr Petrytsya <Vpetrytsia@gmail.com>
2023-05-10 02:22:07 -0400

    base url changed

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,13 +1,10 @@
 import axios from "axios";
 
-API_KEY = "36217922-a5a1e017917ad4e4c89a8ab46";
-BASE_URL = `https://pixabay.com/api/?key=`;
+BASE_URL = `https://pixabay.com/api/?key=36217922-a5a1e017917ad4e4c89a8ab46&q=`;
 PARAMS = `&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=`;
 async function getPictures(searchKeyWord, pageNumber) {
   return await axios.get(
-    `${BASE_URL}${API_KEY}&q=${encodeURIComponent(
-      searchKeyWord
-    )}${PARAMS}${pageNumber}`
+    `${BASE_URL}${encodeURIComponent(searchKeyWord)}${PARAMS}${pageNumber}`
   );
 }
 export default { getPictures };
